1917697e4f1d3dd67b6cdb254a562376392f2147
PC675 <aqayumov96@gmail.com>
2023-02-23 16:03:40 +0500

    co

--- app/src/main/java/uz/itschool/countries/More_info.kt
+++ app/src/main/java/uz/itschool/countries/More_info.kt
@@ -2,12 +2,21 @@ package uz.itschool.countries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.itschool.countries.databinding.ActivityMainBinding
+import uz.itschool.countries.databinding.ActivityMoreInfoBinding
 
 class More_info : AppCompatActivity() {
+    private lateinit var binding: ActivityMoreInfoBinding
+    private var list = mutableListOf<User>()
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_more_info)
+        binding = ActivityMoreInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var adapter = Adapter(this, list)
+
 
 
 
